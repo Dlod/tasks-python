55614c54e996901b4d5d1a4a2b671710dd21bf2f
ma.rozov <ma.rozov@gmail.com>
2019-03-18 10:36:03 +0300

    week 4 task 1 File class

--- week4/week04_01.py
+++ week4/week04_01.py
@@ -1,31 +1,39 @@
-import tempfile, os
+import tempfile, os, uuid
 
 class File:
 
-    def __init__(self, file, content=None):
+    def __init__(self, file):
         self.file = file
-        if content:
-            self.open_file = open(self.file, 'w')
-            self.write(content)
-        self.open_file = open(self.file, 'r+')
+        self.position = 0
+        if not os.path.exists(self.file):
+            open(self.file, 'w').close()
 
-    def __iter__(self):
-        return self.open_file
-
-    def get_content(self):
-        return self.open_file.read()
-
-    def __del__(self):
-        self.open_file.close()
-        del self.open_file
-
-    def write(self, line):
-        self.open_file.write(line)
+    def write(self, content):
+        with open(self.file, "w") as f:
+            f.write(content)
+    
+    def read(self):
+        with open(self.file, 'r') as f:
+            return f.read()
 
     def __add__(self, obj):
-        new_file = File(file=os.path.join(tempfile.gettempdir(), "new_file"),
-        content=self.get_content()+obj.get_content())
+        new_path = os.path.join(tempfile.gettempdir(), uuid.uuid4().hex)
+        new_file = type(self)(new_path)
+        new_file.write(self.read() + obj.read())
         return new_file
 
     def __str__(self):
         return self.file
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        with open(self.file, 'r') as f:
+            f.seek(self.position)
+            line = f.readline()
+            if not line:
+                self.position = 0
+                raise StopIteration('EOF')
+            self.position = f.tell()
+            return line
